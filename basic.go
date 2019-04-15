755964a5ecd7c746da2062e95423312f4914ba42
Zicheng Wang <zicheng_wang2015@hotmail.com>
2019-04-15 16:32:53 -0500

    add PoW

--- basic.go
+++ basic.go
@@ -16,13 +16,14 @@ type Block struct {
 	Hash          []byte
 }
 
+/*
 func (b *Block) SetHash() {
 	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
 	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
 	hash := sha256.Sum256(headers)
 
 	b.Hash = hash[:]
-}
+}*/
 
 func NewBlock(data string, prevBlockHash []byte) *Block {
 	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
