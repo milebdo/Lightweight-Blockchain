f3cf06f314dba564b8aad6fe098b6116220a32d6
quantumcatwang <zicheng_wang2015@hotmail.com>
2019-05-18 18:28:13 -0500

    add consensus algorithm

--- python-blockchain/blockchain.py
+++ python-blockchain/blockchain.py
@@ -1,11 +1,17 @@
+# for basic blockchain data structure
 import hashlib
 import json
 from time import time
 from uuid import uuid4
 from typing import Any, Dict, List, Optional
 
-from textwrap import dedent
+# for Flask web app
+# from textwrap import dedent
 from flask import Flask, jsonify, request
+
+# for consensus algorithms
+from urllib.parse import urlparse
+import requests
 
 
 """
@@ -16,11 +22,13 @@ class Blockchain(object):
 	def __init__(self):
 		self.chain = []
 		self.current_transactions = []
+		self.nodes = set()
 
 		# Create the genesis block
 		self.new_block(previous_hash = '1', proof = 100)
 
 
+	# methods for block produce
 	def new_block(self, proof: int, previous_hash: Optional[str]) -> Dict[str, Any]:
 		"""
 		Create a new block and add to the chain
@@ -76,6 +84,7 @@ class Blockchain(object):
 		return self.chain[-1]
 
 	
+	# methods for PoW
 	def proof_of_work(self, last_proof: int) -> int:
 		"""
 		PoW Algorithm:
@@ -102,6 +111,70 @@ class Blockchain(object):
 		guess = f'{last_proof}{proof}'.encode()
 		guess_hash = hashlib.sha256(guess).hexdigest()
 		return guess_hash[:4] == "0000"
+
+
+	# methods for consensus
+	def register_node(self, address: str) -> Node:
+		"""
+		Add the new nodes to the node list
+		:param address: <str> Address of node. Eg. 'http://192.168.0.5:5000'
+		:return: Node
+		"""
+		parsed_url = urlparse(address)
+		self.nodes.add(parsed_url.netloc)
+
+
+	def valid_chain(self, chain: List[Dict[str, Any]]) -> bool:
+		"""
+		Determine if the given chain is valid
+		:param chain: <List> a given blockchain
+		:return: <bool> True if valid
+		"""
+		last_block = chain[0]
+		current_index = 1
+		while current_index < len(chain):
+			block = chain[current_index]
+			print(f'{last_block}')
+			print(f'{block}')
+			print("\n-----------------------\n")
+			
+			# Check that the hash of the block is correct
+			if block['previous_hash'] != self.hash(last_block):
+				return False
+			
+			# Check the PoW is correct
+			if not self.valid_proof(last_block['proof'], block['proof']):
+				return False
+			
+			last_block = block
+			current_index += 1
+		
+		return True
+
+	
+	def resolve_conficts(self) -> bool:
+		"""
+		when conficts, use the longest chain
+		:return: if replace the chain, return True
+		"""
+		neighbours = self.nodes
+		new_chain = Node
+		max_length = len(self.chain)
+
+		# Grab and verify the chains from all the nodes in our network
+		for node in neighbours:
+			response = requests.get(f'http://{node}/chain')
+			if response.status_code == 200:
+				length = response.json()['length']
+				chain = response.json()['chain']
+				if length > max_length and self.valid_chain(chain):
+					max_length = length
+					new_chain = chain
+		
+		if new_chain:
+			self.chain = new_chain
+			return True
+		return False
 
 
 
@@ -160,7 +233,6 @@ def new_transaction():
 	return jsonify(response), 201
 
 
-
 @app.route('/chain', methods = ['GET'])
 def full_chain():
 	response = {
@@ -170,7 +242,48 @@ def full_chain():
 	return jsonify(response), 200
 
 
+@app.route('/nodes/register', method = ['POST'])
+def register_nodes():
+	values = request.get_json()
+	nodes = values.get('nodes')
+	if nodes is Node:
+		return "Error: please supply a valid list of nodes", 400
 
-# run the node on localhost:5000
+	for node in nodes:
+		blockchain.register_node(node)
+
+	response = {
+		'message': "New nodes have been added",
+		'total nodes': list(blockchain.nodes),
+	}
+	return jsonify(request), 201
+
+
+@app.route('/node/resolve', methods = ['GET'])
+def consensus:
+	replaced = blockchain.register_node()
+
+	if replaced:
+		response = {
+			'message': "Our chain is replaced",
+			'new_chain': blockchain.chain,
+		}
+	else:
+		response = {
+			'message': "Our chain is OK",
+			'chain': blockchain.chain
+		}
+	return jsonify(response), 200
+
+
+
+# run the node on localhost:5000 by default
 if __name__ == '__main__':
-	app.run(host = '0.0.0.0', port = 5000)
+	from argparse import ArgumentParser
+
+	parser = ArgumentParser()
+	parser.add_argument('-p', '--port', default = 5000, type = int, help = 'port to listen on')
+	args = parser.parse_args()
+	port = args.port
+
+	app.run(host = '127.0.0.1', port = port)
